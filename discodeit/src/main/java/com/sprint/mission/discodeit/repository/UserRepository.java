f67f57526a5df0f7a81bbab913a4be62e1c20b0c
구민주 <mj55991188@gmail.com>
2026-05-13 22:54:30 +0900

    Fix: 로그 인 실패 로그 추가 및 에러 수정2

--- discodeit/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +16,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
   Optional<User> findByUsername(String username);
 
+  @EntityGraph(attributePaths = {"profile"})
   Optional<User> findByEmail(String email);
 
   boolean existsByEmail(String email);
